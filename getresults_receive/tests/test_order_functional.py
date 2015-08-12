89dda020dee17ecf7485e28a4d74311e051ae6ab
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-08-12 12:22:00 +0200

    submitting an order functional test

--- getresults_receive/tests/test_order_functional.py
+++ getresults_receive/tests/test_order_functional.py
@@ -1,4 +1,5 @@
 #from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -9,8 +10,8 @@ class TestOrderFunctional(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(3)
         self.browser.get(self.server_url+"receive_order/")
+        time.sleep(3)
 
     def tearDown(self):
 #         self.browser.get(self.server_url+"?testing=remove")
@@ -18,3 +19,31 @@ class TestOrderFunctional(unittest.TestCase):
 
     def test_correct_page(self):
         self.assertIn('Receive Order', self.browser.title)
+
+    def test_order_popup_button(self):
+        order_button = self.browser.find_element_by_id('order-button-0')
+        order_button.click()
+        time.sleep(2)
+        self.switch_to_new_window('Order against aliquot','myModalLabel')
+
+    def test_submit_order_popup(self):
+        order_button = self.browser.find_element_by_id('order-button-0')
+        order_button.click()
+        time.sleep(2)
+        self.switch_to_new_window('Order against aliquot','myModalLabel')
+        submit_button = self.browser.find_element_by_id('order-submit')
+        submit_button.submit()
+        time.sleep(3)
+        self.assertIn('Receive Order', self.browser.title)
+
+    def switch_to_new_window(self, text_in_element, element_id):
+        retries = 60
+        while retries > 0:
+            for handle in self.browser.window_handles:
+                self.browser.switch_to_window(handle)
+                element = self.browser.find_element_by_id(element_id)
+                if text_in_element in element.text:
+                    return
+            retries -= 1
+            time.sleep(0.5)
+        self.fail('could not find pop-up window')

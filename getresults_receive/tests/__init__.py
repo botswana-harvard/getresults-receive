dca6e72f58309690f02e8da99903c29c5cb564aa
erik van widenfelt <ew2789@gmail.com>
2015-08-13 14:07:46 +0200

    rename test module

--- getresults_receive/tests/__init__.py
+++ getresults_receive/tests/__init__.py
@@ -1,4 +1,4 @@
 from .test_receive import TestReceive
-from .test_receive_funtional import TestReceiveFunctional
+from .test_receive_functional import TestReceiveFunctional
 from .test_patient import TestPatient
 from .base_functional_test import BaseFunctionalBase

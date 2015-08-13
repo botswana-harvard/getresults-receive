843112164673c518b515c5cb6c0ca7991a84664a
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-08-13 11:52:43 +0200

    removing items related to Order

--- getresults_receive/tests/__init__.py
+++ getresults_receive/tests/__init__.py
@@ -1,4 +1,4 @@
 from .test_receive import TestReceive
-from .test_order_functional import TestOrderFunctional
 from .test_receive_funtional import TestReceiveFunctional
 from .test_patient import TestPatient
+from .base_functional_test import BaseFunctionalBase

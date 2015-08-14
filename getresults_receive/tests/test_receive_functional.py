1eb82f0e94e4e224578143ae69a46f4fcd7a6280
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-08-14 10:00:46 +0200

    presetting sample_type and protocol number in batch receive

--- getresults_receive/tests/test_receive_functional.py
+++ getresults_receive/tests/test_receive_functional.py
@@ -23,3 +23,20 @@ class TestReceiveFunctional(BaseFunctionalTest):
         self.browser.find_element_by_id('batchbutton').click()
         time.sleep(1)
         self.switch_to_new_window('Receive', 'batchModalLabel')
+
+    def test_receive_batch_pops_up(self):
+        self.test_login()
+        self.browser.get('http://localhost:8000/receive/')
+        receive_batch_button = self.browser.find_element_by_id('receive_batch_button')
+        receive_batch_button.click()
+        time.sleep(1)
+        self.switch_to_new_window('Receive Sample', 'receive_button_id')
+
+    def test_autofill_sample_type(self):
+        self.fail('finish test')
+
+    def test_autofill_protocol_number(self):
+        self.fail('finish test')
+
+    def test_dynamically_change_receive_form_rows(self):
+        self.fail('finish test')

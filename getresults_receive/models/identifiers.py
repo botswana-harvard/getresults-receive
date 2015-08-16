ae069142576bd08e4e36f1cac61686e6255c4dac
erik van widenfelt <ew2789@gmail.com>
2015-08-16 20:51:00 +0200

    remove identifier_history

--- getresults_receive/models/identifiers.py
+++ getresults_receive/models/identifiers.py
@@ -1,7 +1,5 @@
 from getresults_identifier import AlphanumericIdentifier
 
-from getresults.models import IdentifierHistory
-
 
 class ReceiveIdentifier(AlphanumericIdentifier):
 
@@ -9,4 +7,3 @@ class ReceiveIdentifier(AlphanumericIdentifier):
     alpha_pattern = r'^[A-Z]{3}$'
     numeric_pattern = r'^[0-9]{4}$'
     seed = ['AAA', '0000']
-    history_model = IdentifierHistory

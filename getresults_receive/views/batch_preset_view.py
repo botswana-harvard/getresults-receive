98a7d283578b038a308c7bcb126c5c2e6a956686
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-08-17 16:15:05 +0200

    initial receive batch view

--- getresults_receive/views/batch_preset_view.py
+++ getresults_receive/views/batch_preset_view.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 from ..models import Receive
 
@@ -10,25 +11,26 @@ def show_batch(request, **kwargs):
 
 
 class BatchPresetView(TemplateView):
-    template_name = 'reveive.html'
+    template_name = 'receive.html'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+    def get(self, request):
+        #form = self.form_class(initial=self.initial)
+        context = self.get_context_data()
+        return self.render_to_response(context)
+
+    def post(self, request):
+        #form = self.form_class(initial=self.initial)
+        context = self.get_context_data()
+        return self.render_to_response(context)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(
             sections_head='Receive Batch',
             title="Receive Batch",
-            header=[
-                'Batch Identifier',
-                'Patient Identifier',
-                'Receive Identifier',
-                'Collection Datetime',
-                'Receive Datetime',
-                'Sample Type',
-                'Protocol Number',
-            ],
-            batch=kwargs['batch_received'],
+            batch_size=range(1, 5),
         )
         return context

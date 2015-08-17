3b0d5a575cf93aa077f39a53783f0ecfb022c8ff
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-08-17 16:55:40 +0200

    batch form

--- getresults_receive/forms/batch_form.py
+++ getresults_receive/forms/batch_form.py
@@ -1,8 +1,16 @@
-from django.forms import ModelForm
+from django import forms
+from django.utils import timezone
+from datetime import datetime, date
 from ..models import Batch
 
 
-class BatchForm(ModelForm):
+class BatchForm(forms.ModelForm):
+
+    item_count = forms.IntegerField(required=True)
+    collection_date = forms.DateField(required=False)
+    sample_type = forms.CharField(max_length=5, required=False)
+    protocol_numer = forms.CharField(max_length=5, required=False)
+    site_code = forms.CharField(max_length=2, required=False)
 
     class Meta:
         model = Batch

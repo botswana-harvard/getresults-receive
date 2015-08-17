a01ce675175aa9ef517296ee435e93424fecc11c
erik van widenfelt <ew2789@gmail.com>
2015-08-17 13:32:15 +0200

    add missing name to receive identifier

--- getresults_receive/models/identifiers.py
+++ getresults_receive/models/identifiers.py
@@ -3,7 +3,7 @@ from getresults_identifier import AlphanumericIdentifier
 
 class ReceiveIdentifier(AlphanumericIdentifier):
 
-    identifier_type = 'receive'
+    name = 'receiveidentifier'
     alpha_pattern = r'^[A-Z]{3}$'
     numeric_pattern = r'^[0-9]{4}$'
     seed = ['AAA', '0000']

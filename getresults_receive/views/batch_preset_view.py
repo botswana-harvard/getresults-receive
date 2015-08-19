d77c0fce7a8100ab4435629048130e9c097cb419
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-08-19 07:12:35 +0200

    receive batch items

--- getresults_receive/views/batch_preset_view.py
+++ getresults_receive/views/batch_preset_view.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView
 
+from ..models import Batch
+
 from ..forms import BatchForm
+from django.contrib import messages
 
 
 class BatchPresetView(TemplateView):
@@ -23,22 +26,41 @@ class BatchPresetView(TemplateView):
             sample_type = form.cleaned_data.get('sample_type')
             protocol_no = form.cleaned_data.get('protocol_no')
             site_code = form.cleaned_data.get('site_code')
+            batch = None
+            try:
+                batch = Batch(
+                    item_count=item_count,
+                    sample_type=sample_type,
+                    protocol_number=protocol_no,
+                    site_code=site_code,
+                )
+                batch.save()
+            except Exception as err:
+                print(err)
+                batch = Batch.objects.first()
+
             context.update(collection_date_name=collection_date.strftime("%Y-%m-%d") if collection_date else '',
                            sample_type_name=sample_type,
                            protocol_no_name=protocol_no,
                            site_code_name=site_code,
                            batch_size=range(0, item_count),
-                           named_template='receive_batch_items.html'
+                           named_template='receive_batch_items.html',
+                           batch_identifier=batch.batch_identifier,
+                           batch_preset=True,
                            )
             return self.render_to_response(context)
         else:
-            raise
+            message = ''
+            context.update(preset_form=form)
+            context.update(named_template='receive_batch_items.html')
+            messages.add_message(request, messages.INFO, message)
+            return self.render_to_response(context)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(
             sections_head='Receive Batch',
-            title="Receive Batch",
-            batch_size=range(0, 5),
+            batch_preset=True,
+            title="Receive Batch"
         )
         return context
--- getresults_receive/views/receive_view.py
+++ getresults_receive/views/receive_view.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.decorators import login_required
+
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
-from django.shortcuts import render
 
-from ..models import Receive
-from ..models import Batch
+from ..models import Receive, Batch
+
+from getresults_patient.models import Patient
+
+from ..forms import BatchItemForm
+from ..models import BatchItem
+from ..batch_helper import BatchHelper, BatchError
 
 
 class Option(object):
@@ -22,11 +27,22 @@ class ReceiveView(TemplateView):
     template_name = 'receive.html'
 
     def post(self, request):
-        named_template = None
-        if (request.GET.get('action') and (request.GET.get('action') == ('receive' or 'draft'))):
-            # For post requests, the template we will include depends on what the user is attempting to do.
-            named_template = 'receive_batch_items.html'
+        named_template = 'receive_batch_items.html'
         context = self.get_context_data(named_template=named_template)
+        if (request.GET.get('action') and (request.GET.get('action') == ('receive' or 'draft'))):
+
+            if request.POST.get('action') == 'receive':
+                batch_items_list = self.batch_items_list_post(request)
+                if not self.validate_batch_items(batch_items_list):
+                    named_template = 'receive_batch_items.html'
+                    forms = self.batch_item_form_list(batch_items_list)
+                    context.update(batch_item_forms=forms)
+                    context.update(batch_item_status=True)
+                    context.update(batch_preset=False)
+                else:
+                    self.save_valid_batch_items(request.POST.get('patient_identifier'),
+                                                self.batch_items_list_post(request))
+                    context.update(named_template=named_template)
         return self.render_to_response(context)
 
     def get(self, request):
@@ -67,11 +83,13 @@ class ReceiveView(TemplateView):
                 'View': 'View Received samples',
                 'Remove': 'Remove received samples'},
             header_count=3,
-            range_to_receive=range(10),
+            batch_present=False,
             received=Receive.objects.all(),
-            received_count=Receive.objects.all().count()
+            received_count=Receive.objects.all().count(),
+            is_batch_item_valid=False,
         )
         return context
+
 
 #     def get(self, request):
 #         batch = Batch.objects.filter(user_created=request.user.username)
@@ -88,3 +106,67 @@ class ReceiveView(TemplateView):
 #             named_template='my_batch.html'
 #         )
 #         return self.render_to_response(context)
+
+    def field_data(self, index, request, field):
+        field_data = request.POST.getlist(field, None)
+        return field_data[index] if field_data else None
+
+    def batch_items_list_post(self, request):
+        batch_items_list = []
+        for i in range(len(request.POST.getlist('patient_name', []))):
+            form_field = ['patient_name', 'collect_datetime', 'rec_datetime_name', 'sample_type_name',
+                          'protocol_no_name', 'site_code_name']
+            _batch_items_list = []
+            for field in form_field:
+                _batch_items_list.append(self.field_data(i, request, field))
+            batch_items_list.append(dict(patient=_batch_items_list[0], collection_datetime=_batch_items_list[1],
+                                         receive_datetime=_batch_items_list[2], sample_type=_batch_items_list[3],
+                                         protocol_number=_batch_items_list[4], site_code=_batch_items_list[5])
+                                    )
+        return batch_items_list
+
+    def batch_items(self, batch_items_list):
+        """ """
+        batch_items = []
+        for batch_item_data in batch_items_list:
+            batch_item_data.update({'patient': self.patient(batch_item_data.get('patient'))})
+            batch_item = BatchItem.objects.build(**batch_item_data)
+            batch_items.append(batch_item)
+        return batch_items
+
+    def save_valid_batch_items(self, batch_identifier, batch_items_list):
+        batch_helper = BatchHelper(self.batch(batch_identifier))
+        message = "{} Batch items successfully added".format(len(batch_items_list))
+        try:
+            batch_helper.receive_batch(batch_items_list)
+        except BatchError as message:
+            return message
+        return message
+
+    def patient(self, patient_identifier):
+        try:
+            patient = Patient.objects.get(patient_identifier=patient_identifier)
+        except Patient.DoesNotExist:
+            pass
+        return patient
+
+    def batch_item_form_list(self, batch_items_list):
+        batch_items_forms = []
+        for batch_item_data in batch_items_list:
+            batch_item_form = BatchItemForm(data=batch_item_data)
+            batch_items_forms.append(batch_item_form)
+        return batch_items_forms
+
+    def validate_batch_items(self, batch_items_list):
+        for batch_item_form in self.batch_item_form_list(batch_items_list):
+            if not batch_item_form.is_valid():
+                return False
+        return True
+
+    def batch(self, batch_identifier):
+        batch = None
+        try:
+            batch = Batch.objects.get(batch_identifier=batch_identifier)
+        except Batch.DoesNotExist:
+            return Batch.objects.first()  # for development purpose only.
+        return batch
